56ed34e47a35cb1e998b8c5e916fd6224f0a5541
André Filipe de Lima <andrefilipedelima@hotmail.com>
2023-04-06 12:26:08 -0300

    feat: aprendendo sobre injectable services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe-service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,23 +8,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply recipe', 'https://hips.hearstapps.com/hmg-prod/images/easy-dinner-recipes-1676057761.jpeg?crop=1.00xw:0.502xh;0,0.229xh&resize=1200:*'),
-    new Recipe('An Another Recipe', 'This is complex recipe', 'https://www.inspiredtaste.net/wp-content/uploads/2016/07/Pancake-Recipe-2-1200.jpg')
-  ];
 
-  constructor() {
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {
 
   }
 
   ngOnInit() {
-
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  
 
 }
---
+++ src/app/recipes/recipe-service.ts
@@ -0,0 +1,44 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Lasagna', 
+            'Lasagna is both a type of pasta made up of wide ribbons, and also a dish, sometimes called oven lasagna, made with these ribbons placed in layers, and interspersed with stuffing and sauce.', 
+            'https://hips.hearstapps.com/hmg-prod/images/easy-dinner-recipes-1676057761.jpeg?crop=1.00xw:0.502xh;0,0.229xh&resize=1200:*',
+             [
+                new Ingredient('Cheese', 5),
+                new Ingredient('Lasagna pasta', 2),
+                new Ingredient('Tomato Sauce', 1)
+             ]),
+        new Recipe(
+            'A Pancake',
+            'Pancake is a type of dough fried in a little oil on a hot plate or frying pan, basically made with eggs, flour and milk. There are many regional variations of pancakes, some containing yeast or other ingredients.',
+            'https://www.inspiredtaste.net/wp-content/uploads/2016/07/Pancake-Recipe-2-1200.jpg',
+            [
+                new Ingredient('egg', 6),
+                new Ingredient('strawberry', 4),
+                new Ingredient('Sugar', 1),
+                new Ingredient('Milk', 1),
+                new Ingredient('flour', 5)
+            ])
+      ];
+
+      constructor(private shoppingListService: ShoppingListService) {}
+
+      getRecipes() {
+        return this.recipes.slice();
+      }
+
+      addIngredientstoShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+      }
+
+
+}
