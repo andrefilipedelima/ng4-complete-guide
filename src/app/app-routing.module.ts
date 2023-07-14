9824ab0227ec6186b30e0c8dc32f0d216a8c4976
André de Lima <andrefilipedelima@hotmail.com>
2023-07-14 17:17:44 -0300

    aprendendo authentication e Route Protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,13 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch:'full' },
     { path: 'recipes', component: RecipesComponent, 
+        canActivate: [AuthGuard],
         children: [
             { path: '', component: RecipeStartComponent },   
             { path: 'new', component: RecipeEditComponent },
@@ -16,7 +19,8 @@ const appRoutes: Routes = [
             { path: ':id/edit', component: RecipeEditComponent },
         ]
     },
-    { path: 'shopping-list', component: ShoppingListComponent }
+    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }
 ];
 
 @NgModule({
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,22 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable, OnDestroy } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { AuthService } from "../auth/auth.service";
+import { exhaustMap, map, take } from "rxjs/operators";
+import { Subscription } from "rxjs";
 
-@Injectable({providedIn: 'root'})
-export class DataStorageService {
+@Injectable({ providedIn: 'root' })
+export class DataStorageService implements OnDestroy {
 
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    subscription: Subscription;
+
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
+
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -19,16 +29,16 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get('https://ng-course-recipe-book-26205-default-rtdb.firebaseio.com/recipes.json')       
+        this.http.get('https://ng-course-recipe-book-26205-default-rtdb.firebaseio.com/recipes.json')
             .subscribe(
                 (recipesData) => {
                     let recipesArray: Recipe[] = [];
                     for (const key in recipesData) {
                         if (recipesData.hasOwnProperty(key)) {
                             recipesArray = recipesData[key];
-                            if(recipesArray) {
+                            if (recipesArray) {
                                 recipesArray.map(recipe => {
-                                    if(!recipe.ingredients) {
+                                    if (!recipe.ingredients) {
                                         recipe.ingredients = []
                                     }
 
@@ -36,11 +46,13 @@ export class DataStorageService {
                             }
                         }
                     }
-                console.log(recipesArray)
-                this.recipeService.setRecipes(recipesArray);
-            }
-        )
-    
-    } 
+                    console.log(recipesArray)
+                    this.recipeService.setRecipes(recipesArray);
+                }
+            );
+
+
+
+    }
 
 }
